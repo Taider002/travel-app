5864a8f1e983156473bac2eaf79b97daaf34e3d1
Taider <montassartaider002@gmail.com>
2023-10-20 04:23:32 +0200

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 export default function Home() {
@@ -9,6 +11,7 @@ export default function Home() {
       <Camp />
       <Guide />
       <Features />
+      <GetApp />
     </>
   );
 }
